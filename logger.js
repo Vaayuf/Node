c906849a72c4214a148ec7b954ce3e89d685972c
Vaayuf <43188357+Vaayuf@users.noreply.github.com>
2020-02-14 01:48:56 +0530

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,12 @@
+var url = 'http://view.com/view';
+console.log(__filename);
+console.log(__dirname);
+
+function url(message)
+{
+    //send an HTTP request
+    console.log(message);
+}
+
+module.exports.log = url;
+ 
